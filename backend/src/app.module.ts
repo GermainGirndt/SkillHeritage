ccf1893d00d270d274d99e4204bd8275f0318b7e
Germain Girndt <germaingirndt@outlook.com>
2026-01-14 10:26:51 +0100

    feat: add environment configuration and integrate Mongoose for MongoDB connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,27 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoController } from './controllers/video.controller';
 import { InstructionsController } from './controllers/instructions.controller';
 import { TranscriptionController } from './controllers/transcription.controller';
-import { ConfigModule } from '@nestjs/config';
 import { SemanticSearchController } from './controllers/semantic-search.controller';
+import { configValidationSchema } from './configValidationSchema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: configValidationSchema,
+    }),
+
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
     }),
   ],
   controllers: [
--- backend/src/configValidationSchema.ts
+++ backend/src/configValidationSchema.ts
@@ -2,4 +2,6 @@ import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
   SKILL_HERITAGE_VECTOR_STORE_ID: Joi.string().required(),
+  MONGODB_URI: Joi.string().required(),
+  OPENAI_API_KEY: Joi.string().required(),
 });
