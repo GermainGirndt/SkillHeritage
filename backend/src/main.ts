361abe295a1f3b1594cd6ab3f2768f1a4902d20b
Germain Girndt <germaingirndt@outlook.com>
2026-01-26 17:00:08 +0100

    add middleware for logging request

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,13 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 
+const logRequestsMiddleware = (req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+  next();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(logRequestsMiddleware);
+
   app.enableCors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type, Accept, Authorization",
-    credentials: false
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: false,
   });
   app.use('/hls', express.static('dist/assets/videos/hls'));
 
